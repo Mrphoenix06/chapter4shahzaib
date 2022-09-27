37a9f257c33fd5bb75e56450c7e135a6ae7969ab
Mrphoenix06 <109433446+Mrphoenix06@users.noreply.github.com>
2022-09-27 21:57:13 +0500

    Create largest_number.cpp

---
+++ largest_number.cpp
@@ -0,0 +1,17 @@
+#include<iostream> 
+using namespace std;
+#include<conio.h>
+
+int main()
+{
+int a.b.c
+cout<<"Please Enter Three Numbers to Find Largest: ";
+cin>>a>>b>>c;
+if(a>b) if(a>c)
+cout<<endl<<<<"is Largest Number";
+else
+cout<<endl<<c<<"is Largest Number";
+else if(b>c)
+cout<<endl<<b<<"is Largest Number";
+getch();
+}
