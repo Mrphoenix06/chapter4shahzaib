58b9ce59cd40f7d381b96b6aa8b20cf08641d8ab
Mrphoenix06 <109433446+Mrphoenix06@users.noreply.github.com>
2022-09-27 21:54:08 +0500

    Create even_odd.cpp

---
+++ even_odd.cpp
@@ -0,0 +1,13 @@
+#include<iostream> using namespace std;
+#include<conio.h>
+int main()
+{
+int n;
+cout<<"Please Enter a number to check its Even or Odd: ";
+cin>>n;
+if(n%2==0)
+cout<<endl<<"The entered number is Even";
+else
+cout<<endl<<"The entered number is Odd"; 
+return 0;
+}
